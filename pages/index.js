41937480f87276207b974e49732b5373d1f7b041
CastroWill <91801761+CastroWill@users.noreply.github.com>
2024-12-22 19:53:25 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <h1>Entre olhares e sorrisos, encontrei em você meu abrigo. Teus gestos são meu paraíso, teu amor, meu eterno abrigo.❤️</h1>
+  return (
+    <h1>
+      Entre olhares e sorrisos, encontrei em você meu abrigo. Teus gestos são
+      meu paraíso, teu amor, meu eterno abrigo.❤️
+    </h1>
+  );
+}
+
+function teste2() {
+  console.log("teste de indentação");
 }
 
 export default Home;
